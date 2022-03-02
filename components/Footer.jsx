8e7246560fdcdb0071417963ebcb978ab2885ddd
Roxza <39166624+Roxza@users.noreply.github.com>
2022-03-02 20:45:37 +0300

    🐞a little fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <div class="grid grid-cols-1 divide-y divide-[#997c7c] pt-3">
+      <div className="grid grid-cols-1 divide-y divide-[#997c7c] pt-3">
         <div></div>
         <div className="max-w-screen-lg text-white p-5 w-full mx-auto transition-all duration-300">
           <div className="md:flex w-full items-center justify-between">
